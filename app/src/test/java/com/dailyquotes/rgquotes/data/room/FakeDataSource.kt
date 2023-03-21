4f988c841e9b32f37e0edfe74d92780fd3954b43
Ruta Greiliha <ruta.greiliha@gmail.com>
2023-03-21 13:24:35 +0000

    repo testing

--- app/src/test/java/com/dailyquotes/rgquotes/data/room/FakeDataSource.kt
+++ app/src/test/java/com/dailyquotes/rgquotes/data/room/FakeDataSource.kt
@@ -1,19 +1,16 @@
 package com.dailyquotes.rgquotes.data.room
 
-class FakeDataSource(var quoteslist: MutableList<Quote>? = mutableListOf()):QuoteDao {
+class FakeDataSource(var quoteslist: MutableList<Quote>? = mutableListOf()) : QuoteDao {
     override suspend fun getRandomQuoteFromDatabase(): Quote {
         return quoteslist!![0]
-
     }
 
     override suspend fun getSpecificQuoteFromDatabase(quote: String?): Quote {
-        return quoteslist?.find { it.quote==quote }!!
+        return quoteslist?.find { it.quote == quote }!!
     }
 
     override suspend fun repopulateDB(quotes: List<Quote>) {
-        for (quote in quotes){
-            quoteslist?.add(quote)
-        }
+        quoteslist = quotes.toMutableList()
     }
 
     override suspend fun deleteQuoteFromDatabase(quote: String?) {
@@ -21,12 +18,10 @@ class FakeDataSource(var quoteslist: MutableList<Quote>? = mutableListOf()):Quot
     }
 
     override suspend fun roomIsEmpty(): Int {
-        return if (quoteslist != null){
+        return if (quoteslist != null) {
             quoteslist!!.size
-        } else{
+        } else {
             0
         }
     }
-
-
 }
